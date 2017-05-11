c83e08ff32887f0d1cba53edccbabb0e81ed8763
Yevhen Yavorskyi <yyavorskyi@softserveinc.com>
2017-05-11 20:01:34 +0300

    Added Yevhen's files which do not work

---
+++ _NotWorkingYevhenProject_/MFPStarterCordova/platforms/android/assets/www/plugins/cordova-plugin-mfp/worklight/checksum.js
@@ -0,0 +1,1 @@
+var WL_CHECKSUM = {"checksum":976944388,"date":1494521029409,"machine":"ghaili"}
---
+++ _NotWorkingYevhenProject_/MFPStarterCordova/platforms/android/assets/www/plugins/cordova-plugin-mfp/worklight/static_app_props.js
@@ -0,0 +1,20 @@
+// This is a generated file. Do not edit. See application-descriptor.xml.
+// WLClient configuration variables.
+console.log("Running static_app_props.js...");
+var WL = WL ? WL : {};
+WL.StaticAppProps = { APP_ID: 'com.ibm.mfpstartercordova',
+  APP_VERSION: '1.0.1',
+  WORKLIGHT_PLATFORM_VERSION: '8.0.0.00-20170328-120051',
+  WORKLIGHT_NATIVE_VERSION: '2957450250',
+  LANGUAGE_PREFERENCES: 'en',
+  ENVIRONMENT: 'preview',
+  PREVIEW_ENVIRONMENT: 'android',
+  PREVIEW_ROOT_URL: 'http://10.128.225.36:9080/mfp/',
+  APP_SERVICES_URL: '/mfp/apps/services/',
+  POSTFIX_APP_SERVICES_URL: '/mfp/apps/services/',
+  WORKLIGHT_ROOT_URL: '/mfp/apps/services/api/PROJECT/android/',
+  POSTFIX_WORKLIGHT_ROOT_URL: '/mfp/apps/services/api/PROJECT/android/',
+  APP_DISPLAY_NAME: 'PROJECT',
+  LOGIN_DISPLAY_TYPE: 'embedded',
+  mfpClientCustomInit: false,
+  MESSAGES_DIR: 'plugins\\cordova-plugin-mfp\\worklight\\messages' };
