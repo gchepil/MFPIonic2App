c9dd6a0531e320a68fe67a7792dbbf0637603cd2
Grygorii Chepil <gchepil@softserveinc.com>
2017-05-17 15:04:30 +0300

    adding ng1 app

---
+++ apps/NG1App/client/add-user/add-user.controller.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('AddUserController', AddUserController);
+
+    AddUserController.$inject = ['UserServiceMFP', '$location', '$rootScope', 'FlashService'];
+    function AddUserController(UserServiceMFP, $location, $rootScope, FlashService) {
+        var vm = this;
+
+        vm.add = add;
+
+        function add() {
+            vm.dataLoading = true;
+            UserServiceMFP.Create(vm.user)
+                .then(function (response) {
+                    vm.dataLoading = false;
+                    if (response.responseJSON.error) {
+                        FlashService.Error(response.responseJSON.error);
+                    } else {
+                        FlashService.Success('User '+ response.responseJSON.username +' added successfuly', true);
+                        $location.path('/');
+                    }
+                    $rootScope.$digest(); //todo Wlresourse request not call digest !!!!!improve integration
+                });
+        }
+    }
+
+})();
---
+++ apps/NG1App/client/app-services/user.service.js
@@ -0,0 +1,60 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('UserServiceMFP', UserService);
+
+    UserService.$inject = ['$http'];
+    function UserService($http) {
+        var service = {};
+
+        service.GetAll = GetAll;
+        service.GetById = GetById;
+        service.GetByUsername = GetByUsername;
+        service.Create = Create;
+        service.Update = Update;
+        service.Delete = Delete;
+
+        return service;
+
+        function GetAll() {
+            return $http.get('/api/users').then(handleSuccess, handleError('Error getting all users'));
+        }
+
+        function GetById(id) {
+            return $http.get('/api/users/' + id).then(handleSuccess, handleError('Error getting user by id'));
+        }
+
+        function GetByUsername(username) {
+            return $http.get('/api/users/' + username).then(handleSuccess, handleError('Error getting user by username'));
+        }
+
+        function Create(user) {
+            var dataRequest = new WL.ResourceRequest('/adapters/UsersAdapter', WLResourceRequest.POST);
+            dataRequest.addHeader('Content-Type', 'application/json') // todo try to add header globally
+            return dataRequest.send(user);
+        }
+
+        function Update(user) {
+            return $http.put('/api/users/' + user.id, user).then(handleSuccess, handleError('Error updating user'));
+        }
+
+        function Delete(id) {
+            return $http.delete('/api/users/' + id).then(handleSuccess, handleError('Error deleting user'));
+        }
+
+        // private functions
+
+        function handleSuccess(res) {
+            return res.data;
+        }
+
+        function handleError(error) {
+            return function () {
+                return { success: false, message: error };
+            };
+        }
+    }
+
+})();
